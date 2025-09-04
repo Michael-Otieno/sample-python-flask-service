238a8813b7340f69a888097d61d465ee0f13bd18
Michael Otieno <85155606+Michael-Otieno@users.noreply.github.com>
2025-09-05 00:00:45 +0300

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull motieno205/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 motieno205/simple-python-flask-app
+docker run -d -p 5000:5000 motieno205/simple-python-flask-app 
